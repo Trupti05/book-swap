ecaf5686e933144b400dd24c91cfea422aeee183
Trupti Chandwani <210303105014@paruluniversity.ac.in>
2025-10-28 11:57:03 +0530

    updated packages

--- Backend/index.js
+++ Backend/index.js
@@ -25,13 +25,20 @@ app.use(express.json());
 const URI = process.env.MongoDBURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(URI, {
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  try {
+    await mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.log("❌ MongoDB connection error:", err));
+  });
+  console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
+
+connectDB();
 
 // Routes
 app.use("/book", bookRoute);
